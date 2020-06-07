33aee5adfece007b96951a11be318cebd432c81a
RAMANANTSEHENO Tahiana <ramanantsehenotahiana@gmail.com>
2020-06-07 21:22:50 +0300

    reservation

--- application/models/Occupation.php
+++ application/models/Occupation.php
@@ -12,27 +12,6 @@ class Occupation extends CI_Model{
     public function __construct(){
         parent::__construct();
     }
-
-    public function getReservation($token,$db){
-        $sql = "select * from occupation  where valuetoken = ?";
-        $query = $db->query($sql,array($token));
-        $rows = $query->custom_result_object('Occupation');
-        return $rows;
-    }
-
-    public function getReservationToDay(){
-        $sql = "select * from occupation  where valuetoken = ?";
-        $query = $db->query($sql,array($token));
-        $rows = $query->custom_result_object('Occupation');
-        return $rows;
-    }
-
-    public function getReservationRecente(){
-        $sql = "select * from occupation  where valuetoken = ?";
-        $query = $db->query($sql,array($token));
-        $rows = $query->custom_result_object('Occupation');
-        return $rows;
-    }
 }
 
 ?>
--- application/views/reservation.php
+++ application/views/reservation.php
@@ -2,12 +2,20 @@
 	<!-- Three -->
 	<section id="one" class="wrapper">
 		<div class="inner">
-			<div class="flex flex-3">
+		<h2>Vos reservation</h2>
+		<div class="flex flex-<?php echo count($value); ?>">
+		<?php $limit=2; $compteur=0;//permet d'afficher les resultat par 3 ?>
+		<?php if(($status == 'OK') && (count($value) != 0)){ for($i=0; $i<count($value); $i++){ ?>
 				<article>
+					<div class="image fit">
+						<img src="<?php echo base_url(); ?>assets/accueil/images/pic01.jpg" alt="Pic 01" />
+					</div>
 					<header>
-						<h3>Magna tempus sed amet<br /> aliquam veroeros</h3>
+						<h3><?php echo $value[$i]->nomaxe; ?><i class="icon fa-star"></i></h3>
 					</header>
-					<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu.</p>
+					<p style="margin: 0px;" >Date: <?php echo date('d M Y', strtotime($value[$i]->date)); ?></p>
+					<p style="margin: 0px;" >Espace entre voiture: <?php echo $value[$i]->espace; ?> m</p>
+					<p style="margin: 0px;">Duree maximum de stationement: <?php echo $value[$i]->dureemax; ?> h</p>
 					<footer>
 						<form action="<?php echo base_url(); ?>reserver/parckingTerminer" method="post">
 							<input type="hidden" name="idOccupation" value="OCP0013">
@@ -15,36 +23,9 @@
 							<input type="submit" class="button icon fa-times" value="Miala" style="background-color:#e84d4d;">
 						</form>
 					</footer>
-					</footer>
 				</article>
-				<article>
-					<header>
-						<h3>Magna tempus sed amet<br /> aliquam veroeros</h3>
-					</header>
-					<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu.</p>
-					<footer>
-						<form action="<?php echo base_url(); ?>reserver/parckingTerminer" method="post">
-							<input type="hidden" name="idOccupation" value="OCP0013">
-							</br>
-							<input type="submit" class="button icon fa-times" value="Miala" style="background-color:#e84d4d;">
-						</form>
-					</footer>
-					</footer>
-				</article>
-				<article>
-					<header>
-						<h3>Magna tempus sed amet<br /> aliquam veroeros</h3>
-					</header>
-					<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu.</p>
-					<footer>
-						<form action="<?php echo base_url(); ?>reserver/parckingTerminer" method="post">
-							<input type="hidden" name="idOccupation" value="OCP0013">
-							</br>
-							<input type="submit" class="button icon fa-times" value="Miala" style="background-color:#e84d4d;">
-						</form>
-					</footer>
-					</footer>
-				</article>
+		<?php }
+			}else{ echo $message; } ?>
 			</div>
 		</div>
 	</section>
