f42ada7eec2051aa655005c43e5d7c8655c1b9ae
Manohisoa Randriambahiny <randriambahinymanohy@gmail.com>
2020-06-07 14:07:13 +0300

    login inscription

--- application/controllers/back.php
+++ application/controllers/back.php
@@ -1,14 +1,113 @@
-<?php 
-   class Back extends CI_Controller 
+<?php
+class Back extends CI_Controller
+{
+   function __construct()
    {
-      function __construct() 
-      { 
-         parent::__construct(); 
-         $this->load->helper('url'); 
-      } 
-      public function index() 
-      {
-         $this->load->view('login'); 
+      parent::__construct();
+      $this->load->helper('url');
+      $this->load->database();
+      $this->load->model('Users');
+      $this->load->model('Util');
+      session_start();
+   }
+   public function index()
+   {
+      if (isset($_SESSION['user'])) {
+         //redirect to back accueil
+      } else {
+         $data['page'] = "login";
+         $this->load->view('back/auth', $data);
       }
-    }
-?>
+   }
+   public function InscriptionPage()
+   {
+      $data['page'] = "inscription";
+      $this->load->view('back/auth', $data);
+   }
+   public function login()
+   {
+      $data = array();
+      try {
+
+         $nom = $this->input->post('nom');
+         $mdp = $this->input->post('mdp');
+         
+         if ($nom === "") throw new Exception("nom invalide");
+         else if ($mdp == "") throw new Exception("Mot de passe tsy normal");
+         
+         $query = $this->db->query("select * from users where nom='".$nom."'");
+         $rows = $query->custom_result_object('Users');
+
+         $pers=null;
+         foreach ($rows as $row) {
+            $pers = $row;
+         }
+         if($pers==null){
+            throw new Exception("nom invalide");
+         }else{
+            if(password_verify($mdp,$pers->mdp)){
+               $_SESSION['user']=$pers->id;
+               //redirect to back accueil
+            }else{
+               throw new Exception("Mot de passa diso");
+            }
+         }
+
+      } catch (Exception $ex) {
+
+         $data['error'] = $ex->getMessage();
+         $data['page'] = "login";
+         $this->load->view('back/auth', $data);
+      }
+      
+   }
+   public function logOut()
+   {
+      if (isset($_SESSION['user'])) $_SESSION['user'] = null;
+      session_destroy();
+      $data['page'] = "login";
+      $this->load->view('back/auth', $data);
+   }
+   public function Inscription()
+   {
+      $data = array();
+      try {
+         $this->db->trans_begin();
+         $nom = $this->input->post('nom');
+         $mdp = $this->input->post('mdp');
+         $confmdp = $this->input->post('ConfirMdp');
+         if ($nom === "") throw new Exception("nom invalide");
+         else if ($mdp == "") throw new Exception("Mot de passe tsy normal");
+         else if ($mdp != $confmdp) throw new Exception("Mot de passe tsy mitovy");
+
+         $requette = "select count(*) as nb from users where nom='" . $nom . "'";
+         $query = $this->db->query($requette);
+         $nb = 0;
+         foreach ($query->result_array() as $row) {
+            $nb = $row['nb'];
+         }
+         if ($nb == 0) {
+            $id = "USR" . $this->Util->formatNumber($this->Util->getNextVal('seq_users'), 4);
+            $passHash = password_hash($mdp, PASSWORD_BCRYPT);
+            $dataInsert = array(
+               'id' => $id,
+               'nom' => $nom,
+               'mdp' => $passHash
+
+            );
+
+            $this->db->insert('users', $dataInsert);
+            $this->db->trans_commit();
+            $data['page'] = "login";
+            $this->load->view('back/auth', $data);
+         } else {
+            throw new Exception("Nom efa ao");
+         }
+      } catch (Exception $ex) {
+         $this->db->trans_rollback();
+         $data['error'] = $ex->getMessage();
+         $data['page'] = "inscription";
+         $this->load->view('back/auth', $data);
+      }
+   }
+}
---
+++ application/views/back/auth.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign Up Form by Colorlib</title>
+
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/login/fonts/material-icon/css/material-design-iconic-font.min.css">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/login/css/style.css">
+</head>
+<body>
+
+    <div class="main">
+
+        <section class="signup">
+            
+            <div class="container">
+                <?php if($page=="login"){?> 
+                    <div class="signup-content">
+                    <?php if(isset($error)){ ?> 
+                        <h4 style="color:red;"><?php echo $error; ?></h4>
+                    <?php } ?>
+                    <form method="POST" action="<?php echo base_url(); ?>back/login" id="signup-form" class="signup-form">
+                        <h2 class="form-title">Laoginina</h2>
+                        <div class="form-group">
+                            <input type="text" class="form-input" name="nom" id="name" placeholder="Your Name"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-input" name="mdp" id="password" placeholder="Password"/>
+                            <span toggle="#password" class="zmdi zmdi-eye field-icon toggle-password"></span>
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" name="submit" id="submit" class="form-submit" value="Hiditra"/>
+                        </div>
+                    </form>
+                    <p class="loginhere">
+                       Mbola tsy misy Kaonty ? <a href="<?php echo base_url(); ?>back/InscriptionPage" class="loginhere-link">Mamorona eto</a>
+                    </p>
+                </div>
+               <?php }else{ ?>
+               
+                <div class="signup-content">
+                <?php if(isset($error)){ ?> 
+                    <h4 style="color:red;"><?php echo $error; ?></h4>
+                <?php } ?>
+                    <form method="POST" id="signup-form" action="<?php echo base_url(); ?>back/Inscription" class="signup-form">
+                        <h2 class="form-title">Mamorona Kaonty</h2>
+                        <div class="form-group">
+                            <input type="text" class="form-input" name="nom" id="name" placeholder="Anarana"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-input" name="mdp" id="password" placeholder="Teny Miafina"/>
+                            <span toggle="#password" class="zmdi zmdi-eye field-icon toggle-password"></span>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-input" name="ConfirMdp" id="re_password" placeholder="Avereno ny teny  Miafina"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" name="submit" id="submit" class="form-submit" value="Foronina"/>
+                        </div>
+                    </form>
+                    <p class="loginhere">
+                       Efa manana Kaonty ? <a href="<?php echo base_url(); ?>back" class="loginhere-link">Laoginina eto</a>
+                    </p>
+                <?php } ?>
+                
+            </div>
+        </section>
+
+    </div>
+
+    <!-- JS -->
+    <script src="<?php echo base_url();?>assets/login/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url();?>assets/login/js/main.js"></script>
+</body>
+</html>
